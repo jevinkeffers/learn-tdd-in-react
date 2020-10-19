88c821a97f519daf44cec30ed4e19600da5a934a
Kevin Jeffers <jevinkeffers@gmail.com>
2020-10-19 14:04:24 -0400

    TDD in React

---
+++ cypress/integration/creating_a_message.spec.js
@@ -0,0 +1,13 @@
+describe('Creating a message', () => {
+    it('Displays the message in a list', () => {
+        cy.visit('http://localhost:3000');
+
+        cy.get('[data-testid="messageText"]').type('New Message');
+
+        cy.get('[data-testid="sendButton"]').click();
+
+        cy.get('[data-testid="messageText"]').should('have.value', '');
+
+        cy.contains('New Message');
+    })
+})
